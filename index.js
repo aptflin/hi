6a296464a662ac1d2ff955841bd29f19e5199256
aptflin <110479461+aptflin@users.noreply.github.com>
2025-06-05 11:22:14 +0800

    fix html

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-// 把整個根目錄設為可讀取靜態檔案
+// Serve all static files from current directory
 app.use(express.static(__dirname));
 
-// 將首頁導向 player_ready/player_r.html
+// Send the main page on /
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'player_ready', 'player_r.html'));
 });
